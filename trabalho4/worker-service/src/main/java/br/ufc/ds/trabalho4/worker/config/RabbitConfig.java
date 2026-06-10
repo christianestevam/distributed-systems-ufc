55153f04657daebe2463245e1ecd564bec0b312f
Christian Estevam <christianestevamb@gmail.com>
2026-06-09 23:19:21 -0300

    implementa dlq

--- trabalho4/worker-service/src/main/java/br/ufc/ds/trabalho4/worker/config/RabbitConfig.java
+++ trabalho4/worker-service/src/main/java/br/ufc/ds/trabalho4/worker/config/RabbitConfig.java
@@ -4,13 +4,26 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class RabbitConfig {
 
     public static final String QUEUE_NAME = "bolsa.mensagens";
+    public static final String DLQ_NAME = QUEUE_NAME + ".dlq";
 
     @Bean
     public Queue bolsaQueue() {
-        return new Queue(QUEUE_NAME, true);
+        Map<String, Object> args = new HashMap<>();
+        // route dead letters to the DLQ via default exchange using routing key = DLQ_NAME
+        args.put("x-dead-letter-exchange", "");
+        args.put("x-dead-letter-routing-key", DLQ_NAME);
+        return new Queue(QUEUE_NAME, true, false, false, args);
+    }
+
+    @Bean
+    public Queue bolsaDlq() {
+        return new Queue(DLQ_NAME, true);
     }
 }
